ee5030847b6accbaea564f99bb6cd4d4c9acaa35
Leo Moura <leo.moura@futurevaultinc.com>
2020-03-10 07:51:26 -0400

    Adding user form

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import {Button} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
-import {Container} from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {Container, Form, Input, SubmitButton} from './styles';
 
 export default function Main() {
   const navigation = useNavigation();
+  navigation.setOptions({
+    title: 'Users',
+  });
 
   return (
     <Container>
-      <Button
-        title="User"
-        onPress={() => {
-          navigation.navigate('User');
-        }}
-      />
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Add User"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#FFF" />
+        </SubmitButton>
+      </Form>
     </Container>
   );
 }
