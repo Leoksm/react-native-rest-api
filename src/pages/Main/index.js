e23dc439249f5994a5c7b031f5795575c264b76b
Leo Moura <leo.moura@futurevaultinc.com>
2020-03-14 12:43:59 -0400

    Adding HandleAddUser

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import {Button} from 'react-native';
+import React, {useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {Container, Form, Input, SubmitButton} from './styles';
 
 export default function Main() {
+  const [newUser, setNewUser] = useState('');
+  const [users, setUsers] = useState([]);
   const navigation = useNavigation();
   navigation.setOptions({
     title: 'Users',
   });
+
+  function handleAddUser() {
+    return console.tron.log(newUser);
+  }
 
   return (
     <Container>
@@ -17,8 +22,12 @@ export default function Main() {
           autoCorrect={false}
           autoCapitalize="none"
           placeholder="Add User"
+          value={newUser}
+          onChangeText={text => setNewUser(text)}
+          returnKeyType="send"
+          onSubmitEditing={handleAddUser}
         />
-        <SubmitButton>
+        <SubmitButton onPress={handleAddUser}>
           <Icon name="add" size={20} color="#FFF" />
         </SubmitButton>
       </Form>
